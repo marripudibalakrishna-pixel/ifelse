9bffed543bd862ec939dd18d7ad5960003cb306a
ADMIN <ADMIN@DESKTOP-O0R2CLM>
2026-01-04 02:53:21 +0530

    Add project files.

---
+++ ifelse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int a =int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("The first number is greater than the second number.");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("The first number is less than the second number.");
+            }
+            else
+            {
+                Console.WriteLine("The two numbers are equal.");
+            }
+
+
+
+        }
+    }
+}
